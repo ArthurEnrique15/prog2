4f49455e43555ef1c2248084c6395a9bb76bc561
Arthur Silva <arthurenrique1512@gmail.com>
2021-05-29 16:34:54 -0300

    exercicios 02 e 03 do trabalho 01

---
+++ TP01/Exercicio_02/src/Planta.java
@@ -0,0 +1,11 @@
+public class Planta {
+    String nomePlanta;
+    int qtdEstoque;
+    int estoqueIdeal;
+
+    public Planta () {
+        nomePlanta = "";
+        qtdEstoque = 0;
+        estoqueIdeal = 0;
+    }
+}
---
+++ TP01/Exercicio_03/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n, resultado;
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Digite o número: ");
+
+        n = scn.nextInt();
+
+        resultado = funcao(n);
+
+        System.out.println("F(" + n + ") = " + resultado);
+    }
+
+    public static int funcao (int n) {
+        if (n == 1)
+            return 1;
+
+        if (n == 2)
+            return 2;
+
+        int resultado = (2 * funcao(n-1)) + (3 * funcao(n-2));
+        return resultado;
+    }
+}
